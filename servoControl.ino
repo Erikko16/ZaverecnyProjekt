88a5502ee3faa3e3ccdc2963b5afcbb552817bb7
Erikko16 <ddffdd04@gmail.com>
2019-11-03 17:16:59 +0100

    Create servoControl.ino

---
+++ servoControl.ino
@@ -0,0 +1,18 @@
+#include <Servo.h>
+
+Servo myservo;  
+
+int pos = 0;    
+void setup() {
+  myservo.attach(5);  
+
+void loop() {
+  for (pos = 0; pos <= 180; pos += 1) { 
+    myservo.write(pos);              
+    delay(1);                       
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { 
+    myservo.write(pos);             
+    delay(15);                      
+  }
+}
