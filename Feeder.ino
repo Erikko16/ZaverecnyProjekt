7ed4208f643c915153b16c6d3239ad796d912633
Erikko16 <ddffdd04@gmail.com>
2019-11-04 12:34:45 +0100

    Update Feeder.ino

--- Feeder.ino
+++ Feeder.ino
@@ -10,7 +10,7 @@ const long utcOffsetInSeconds = 3600;
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+NTPClient timeClient(ntpUDP, "tik.cesnet.cz", utcOffsetInSeconds);
 
 void setup(){
   Serial.begin(115200);
